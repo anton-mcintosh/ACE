3c7fd5749cd4a177b0c7904cbf3e0c1f5b583ee3
Johan <jjdouma99@gmail.com>
2024-03-01 11:56:49 -0800

    Wrote some stuff in Settings.js

--- ACE/app/Screens/Settings.js
+++ ACE/app/Screens/Settings.js
@@ -1,1 +1,39 @@
+/* Settings Screen */
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+// import { useLocalSearchParams, Link, router } from 'expo-router';}
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Button from '../components/Button';
+
+const settings = () => {
+    return (
+        <view style ={styles.header}>
+            <text style ={styles.text}>Settings & Customization</text>
+            <StatusBar style = 'auto'/>
+        </view>
+    )
+}
+
+export default settings;
+
+const styles = StyleSheet.create({
+    header: {
+        alignItems: 'center',
+        width: '100%',
+        marginTop: 10,
+        marginBottom: 20,
+    },  
+    container: {
+        flex: 1,
+        backgroundColor: '#000000',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 30,
+        color: '#ffffff',
+    },
+
+
+});
