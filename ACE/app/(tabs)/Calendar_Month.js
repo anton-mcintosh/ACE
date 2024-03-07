175f06042e2084b480cbad030870b0c5ce28c525
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-03-07 07:29:05 -0800

    most recent calendar change, showing daily stuff

--- ACE/app/(tabs)/Calendar_Month.js
+++ ACE/app/(tabs)/Calendar_Month.js
@@ -8,26 +8,39 @@ import {
   TouchableOpacity,
   SafeAreaView,
 } from "react-native";
-import { Calendar } from "react-native-calendars";
+import { Agenda, Calendar } from "react-native-calendars";
 //import * as Calendar from "expo-calendar";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Calendar_Month = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.calendar}>
-        <Calendar />
-      </View>
+    <SafeAreaView style={styles.container}>
+      <Agenda
+        selected="2022-12-01"
+        items={{
+          "2022-12-01": [
+            { name: "Cycling" },
+            { name: "Walking" },
+            { name: "Running" },
+          ],
+          "2022-12-02": [{ name: "Writing" }],
+        }}
+        renderItem={(item, isFirst) => (
+          <TouchableOpacity style={styles.item}>
+            <Text style={styles.itemText}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </SafeAreaView>
   );
 };
 export default Calendar_Month;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "space-around",
+    justifyContent: "center",
   },
   header: {
     flexDirection: "row",
@@ -39,5 +52,9 @@ const styles = StyleSheet.create({
   },
   calendar: {
     marginTop: 30,
+  },
+  agendaList: {
+    flex: 1,
+    marginTop: 20,
   },
 });
